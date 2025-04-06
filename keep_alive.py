2c78ba314a808d3c2cb7594dcfdd9b7ea422f782
denus123 <denusvelenchuk@gmail.com>
2025-04-06 03:48:40 -0400

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,17 @@
 
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "I'm alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
